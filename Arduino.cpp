a043974a3006d5268dcfc2e934572908dadcca9d
Nic Newdigate <nicnewdigate@gmail.com>
2020-12-27 15:05:03 +0000

    moved to own repo

---
+++ Arduino.cpp
@@ -0,0 +1,34 @@
+#include <sys/timeb.h>
+#include "Arduino.h"
+#include <unistd.h>
+#include <sys/time.h>
+timeb t_start;
+unsigned tv_start_unsigned;
+
+unsigned long millis() {
+  timeb t_now;
+  ftime(&t_now);
+  return (t_now.time  - t_start.time) * 1000 + (t_now.millitm - t_start.millitm);
+}
+
+unsigned long micros() {
+  struct timeval tv_now;
+  gettimeofday(&tv_now,NULL);
+  unsigned long time_in_micros = 1000000 * tv_now.tv_sec + tv_now.tv_usec;
+  return (time_in_micros - tv_start_unsigned);
+}
+
+void delay(unsigned long ms) {
+  unsigned long start = millis();
+  while(millis() - start < ms){
+    usleep(1000);
+  }
+}
+
+void initialize_mock_arduino() {
+  ftime(&t_start);
+  struct timeval tv_start;
+  gettimeofday(&tv_start,NULL);
+
+  tv_start_unsigned = 1000000 * tv_start.tv_sec + tv_start.tv_usec;
+}
---
+++ Arduino.h
@@ -0,0 +1,20 @@
+#pragma once
+
+typedef unsigned char byte;
+typedef unsigned short int word;
+
+unsigned long millis();
+unsigned long micros();
+void delay(unsigned long ms);
+
+#define bitRead(value, bit) (((value) >> (bit)) & 0x01)
+#define bitSet(value, bit) ((value) |= (1UL << (bit)))
+#define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
+#define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
+
+// WMath.cpp
+long map(long, long, long, long, long);
+
+void initialize_mock_arduino(); 
+
+#include "hardware_serial.h"
