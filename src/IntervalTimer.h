3a2dbbc34c6f30b4659a2e1577468e4394ccbbbc
Newdigate <nicnewdigate@gmail.com>
2023-12-19 11:53:14 +0000

    #include <functional>

--- src/IntervalTimer.h
+++ src/IntervalTimer.h
@@ -37,7 +37,7 @@
 #include <signal.h>
 #include <time.h>
 #include <sys/time.h>
-
+#include <functional>
 // IntervalTimer provides access to hardware timers which can run an
 // interrupt function a precise timing intervals.
 // https://www.pjrc.com/teensy/td_timing_IntervalTimer.html
